77edf65c3011d032fdf5b72bdf07272db8408cbb
Suniksha <sunikshabenpatel@gmail.com>
2024-10-19 01:54:25 +0530

    Day-1

---
+++ Day-1/sign.php
@@ -0,0 +1,12 @@
+<!--Registration Form - 1) Signup 
+                        2) Login 
+                        3) Welcome
+                        4) Log Out
+                        
+    1) Username , password , Signup
+       Condition : if user already exist dont insert data 
+                   if user doent exist then signup successfully
+                   
+    2) Username , password , Login Button
+       Condition : if username and password match the login successfully and land to welcome page
+                   if username and passowrd doesnt match then according to the data return invalid credentials.-->
