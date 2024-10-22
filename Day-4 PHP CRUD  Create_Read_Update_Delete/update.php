d2298e4129f36990fc219dd18bd61600697f3ac6
Suniksha <sunikshabenpatel@gmail.com>
2024-10-22 16:52:44 +0530

    Day-4

--- Day-4 PHP CRUD  Create_Read_Update_Delete/update.php
+++ Day-4 PHP CRUD  Create_Read_Update_Delete/update.php
@@ -1,6 +1,14 @@
 <?php
 include('connect.php');
 $id = $_GET['updateid'];
+$sql = "SELECT * FROM `crud` where id=$id";
+$result = mysqli_query($con, $sql);
+$row=mysqli_fetch_assoc($result);
+
+$name = $row['name'];
+$email = $row['email'];
+$mobile = $row['mobile'];
+$password = $row['password'];
 if(isset($_POST['submit'])){
     $name =$_POST['name'];
     $email=$_POST['email'];
@@ -35,19 +43,19 @@ if(isset($_POST['submit'])){
        <form method="post">
         <div class="mb-3">
            <label>Name</label>
-           <input type="text" class="form-control" placeholder="Enter your name" name="name" autocomplete="off">
+           <input type="text" class="form-control" placeholder="Enter your name" name="name" autocomplete="off" value=<?php echo $name;?>>
         </div>
         <div class="mb-3">
            <label>Email</label>
-           <input type="email" class="form-control" placeholder="Enter your email" name="email" autocomplete="off">
+           <input type="email" class="form-control" placeholder="Enter your email" name="email" autocomplete="off" value=<?php echo $email;?>>
         </div>
         <div class="mb-3">
            <label>Mobile</label>
-           <input type="text" class="form-control" placeholder="Enter your mobile number" name="mobile" autocomplete="off">
+           <input type="text" class="form-control" placeholder="Enter your mobile number" name="mobile" autocomplete="off" value=<?php echo $mobile;?>>
         </div>
         <div class="mb-3">
            <label>Password</label>
-           <input type="text" class="form-control" placeholder="Enter your password" name="password" autocomplete="off">
+           <input type="text" class="form-control" placeholder="Enter your password" name="password" autocomplete="off" value=<?php echo $password;?>>
         </div>
 
       <button type="submit" class="btn btn-primary" name="submit">Update</button>
