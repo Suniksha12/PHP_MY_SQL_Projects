49a520a65854554f4eb76a5b057bf9d8e2f6e1cb
Suniksha <sunikshabenpatel@gmail.com>
2024-10-23 11:21:57 +0530

    Day-5

--- Day-5 Complete Dynamic Website using HTML,PHP ,MYSQL and Bootstrap/partials/header.php
+++ Day-5 Complete Dynamic Website using HTML,PHP ,MYSQL and Bootstrap/partials/header.php
@@ -1,11 +1,11 @@
 <nav class="navbar navbar-expand-lg navbar-light py-0">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">StoriesFun</a>
+    <a class="navbar-brand" href="index.php">StoriesFun</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav ml-auto">
+      <ul class="navbar-nav ms-auto">
         <li class="nav-item active">
           <a class="nav-link" aria-current="page" href="#">Home
           </a>
