991b4ae1c97965df36387288a6696f5b2c1fa4b9
Suniksha <sunikshabenpatel@gmail.com>
2024-10-27 21:30:12 +0530

    Day-9

--- Day-6 PHP CRUD using Bootstrap Modal Complete Project AJAX JQUERY/index.php
+++ Day-6 PHP CRUD using Bootstrap Modal Complete Project AJAX JQUERY/index.php
@@ -10,11 +10,50 @@
 <body>
     <div class="container my-3">
         <h1 class="text-center">PHP CRUD operations Using Bootstrap Modal</h1>
-        <button class="btn btn-dark">Add New Users</button>
+        <button type="button" class="btn btn-dark my-4" data-bs-toggle="modal" data-bs-target="#completeModal">
+            Add New Users
+        </button>
     </div>
+
+    <!-- Modal -->
+    <div class="modal fade" id="completeModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h1 class="modal-title fs-5" id="exampleModalLabel">New User</h1>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+            <div class="mb-3 form-group">
+              <label for="completename" class="form-label">Name</label>
+              <input type="text" class="form-control" id="completename" placeholder="Enter your name">
+            </div>
+            <div class="mb-3 form-group">
+              <label for="completemail" class="form-label">Email</label>
+              <input type="email" class="form-control" id="completemail" placeholder="Enter your email">
+            </div>
+            <div class="mb-3 form-group">
+              <label for="completemobile" class="form-label">Mobile</label>
+              <input type="text" class="form-control" id="completemobile" placeholder="Enter your mobile Number">
+            </div>
+            <div class="mb-3 form-group">
+              <label for="completeplace" class="form-label">Place</label>
+              <input type="text" class="form-control" id="completeplace" placeholder="Enter your Place">
+            </div>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary">Save changes</button>
+          </div>
+        </div>
+      </div>
+    </div>
+
     <!-- Bootstrap JavsScript-->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"></script> 
 </body>
 </html>
+
+
