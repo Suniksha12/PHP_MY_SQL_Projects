df798d74a010fe33c48b9f747270c4999ed51d7e
Suniksha <sunikshabenpatel@gmail.com>
2024-10-31 21:54:47 +0530

    Day-12

--- Day-6 PHP CRUD using Bootstrap Modal Complete Project AJAX JQUERY/display.php
+++ Day-6 PHP CRUD using Bootstrap Modal Complete Project AJAX JQUERY/display.php
@@ -4,10 +4,26 @@
     $table='<table class="table">
         <thead class="thead-dark">
         <tr>
-           <th scope="col">#</th>
-           <th scope="col">#</th>
-           <th scope="col">#</th>
+           <th scope="col">Sl no</th>
+           <th scope="col">Name</th>
+           <th scope="col">Email</th>
+           <th scope="col">Mobile</th>
+           <th scope="col">Operations</th>
         </tr>
         </thead>';
+
+        $sql = "SELECT * FROM `crud`";
+
+        $result = mysqli_query($con,$sql);
+
+        while($row=mysqli_fetch_assoc($result)){
+            $id=$row['id'];
+            $table.= '<tr>
+               <td scope="row">1</td>
+               <td></td>
+               <td></td>
+               <td></td>
+            </tr>';
+        }
    }
 ?>
