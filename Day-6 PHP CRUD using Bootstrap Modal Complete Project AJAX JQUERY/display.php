00cbfa0f3450687523eaf7ce34be7928935065f3
Suniksha <sunikshabenpatel@gmail.com>
2024-10-31 21:48:39 +0530

    Day-12

--- Day-6 PHP CRUD using Bootstrap Modal Complete Project AJAX JQUERY/display.php
+++ Day-6 PHP CRUD using Bootstrap Modal Complete Project AJAX JQUERY/display.php
@@ -1,4 +1,13 @@
 <?php
    include 'connect.php';
-   if(isset($_POST['displaySend']))
+   if(isset($_POST['displaySend'])){
+    $table='<table class="table">
+        <thead class="thead-dark">
+        <tr>
+           <th scope="col">#</th>
+           <th scope="col">#</th>
+           <th scope="col">#</th>
+        </tr>
+        </thead>';
+   }
 ?>
