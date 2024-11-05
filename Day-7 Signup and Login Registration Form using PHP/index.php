c2993275620686415c2750de0ef2f9a5759b4c8d
Suniksha <sunikshabenpatel@gmail.com>
2024-11-06 01:52:48 +0530

    Day-17

---
+++ Day-7 Signup and Login Registration Form using PHP/index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Registration Project</title>
+  </head>
+  <body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 col-xm-12">
+                
+            </div>
+        </div>
+    </div>
+  </body>
+</html>
