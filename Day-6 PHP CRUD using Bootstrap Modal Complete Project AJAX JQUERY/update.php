ba119633e2a5c8e842fbfcc26995ccf087038ea2
Suniksha <sunikshabenpatel@gmail.com>
2024-11-05 08:37:12 +0530

    Day-16

--- Day-6 PHP CRUD using Bootstrap Modal Complete Project AJAX JQUERY/update.php
+++ Day-6 PHP CRUD using Bootstrap Modal Complete Project AJAX JQUERY/update.php
@@ -6,5 +6,10 @@
        $sql = "SELECT * FROM `crud` WHERE id=$user_id";
 
        $result = mysqli_query($con,$sql);
+       $response=array();
+       while($row=mysqli_fetch_assoc($result)){
+        $response = $row;
+       }
+      echo json_encode($response);
    }
 ?>
