415ede9f71a19dc811e670ecb49d8c4d1ef395c4
Suniksha <sunikshabenpatel@gmail.com>
2024-11-06 09:18:52 +0530

    Day-17

--- Day-7 Signup and Login Registration Form using PHP/index.php
+++ Day-7 Signup and Login Registration Form using PHP/index.php
@@ -11,21 +11,35 @@
     <div class="container my-5">
         <div class="row">
             <div class="col-md-6 col-xm-12">
-                
+                <h1 class="text-center">Sign up</h1>
                 <form>
                     <div class="mb-3">
                         <label for="username" class="form-label">Username</label>
-                        <input type="text" class="form-control" id="username" placeholder="Enter username" autocomplete="off">
+                        <input type="text" class="form-control" id="username" placeholder="Enter username" autocomplete="off" name="username">
                     </div>
                     <div class="mb-3">
                         <label for="password" class="form-label">Password</label>
-                        <input type="password" class="form-control" id="password" placeholder="Enter password" autocomplete="off">
+                        <input type="password" class="form-control" id="password" placeholder="Enter password" autocomplete="off" name="password">
                     </div>
                     <div class="mb-3">
                         <label for="cpassword" class="form-label">Confirm password</label>
-                        <input type="password" class="form-control" id="cpassword" placeholder="Enter Confirm password" autocomplete="off">
+                        <input type="password" class="form-control" id="cpassword" placeholder="Enter Confirm password" autocomplete="off" name="cpassword">
                     </div>
-                    <button type="submit" class="btn btn-success w-100">Sign up</button>
+                    <button type="submit" class="btn btn-success w-100 my-3">Sign up</button>
+                </form>
+            </div>
+            <div class="col-md-6 col-xm-12 my-5">
+                <h1 class="text-center">Login</h1>
+                <form>
+                    <div class="mb-3">
+                        <label for="username" class="form-label">Username</label>
+                        <input type="text" class="form-control" id="username" placeholder="Enter username" autocomplete="off" name="username">
+                    </div>
+                    <div class="mb-3">
+                        <label for="password" class="form-label">Password</label>
+                        <input type="password" class="form-control" id="password" placeholder="Enter password" autocomplete="off" name="password">
+                    </div>
+                    <button type="submit" class="btn btn-dark w-100 my-3">Login up</button>
                 </form>
             </div>
         </div>
