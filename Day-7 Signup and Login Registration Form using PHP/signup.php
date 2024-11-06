7cfe216cf0fc05d7c01ac9346251f3a81eed6b20
Suniksha <sunikshabenpatel@gmail.com>
2024-11-07 01:27:43 +0530

    Day-18

--- Day-7 Signup and Login Registration Form using PHP/signup.php
+++ Day-7 Signup and Login Registration Form using PHP/signup.php
@@ -1,11 +1,12 @@
 <?php 
+  $user =0;
   if($_SERVER['REQUEST_METHOD']=='POST'){
     include 'connect.php';
 
     if(isset($_POST['signup'])){
         $username = $_POST['username'];
-        $password = $_POST['password'];
-        $cpassword = $_POST['cpassword'];
+        $password = md5($_POST['password']);
+        $cpassword = md5($_POST['cpassword']);
 
         // $sql = "INSERT INTO `rdata` (username,password) values('$username','$password')";
         // $result = mysqli_query($con,$sql);
@@ -14,12 +15,14 @@
         // } else {
         //     die(mysqli_error($con));
         // }
+
        $sql = "SELECT * FROM `data` WHERE username='$username'";
        $result = mysqli_query($con,$sql);
        if($result){
         $num = mysqli_num_rows($result);
         if($num>0){
-            echo "User already exist";
+            // echo "User already exist";
+            $user = 1;
         } else {
             if($password === $cpassword){
             $sql = "INSERT INTO `data` (username,password) values('$username','$password')";
@@ -35,3 +38,22 @@
     }
   }
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- styling CSS-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>Sign up Page</title>
+</head>
+<body>
+    <?php 
+      if($user){
+        echo "<div class='alert alert-danger' role='alert'> User Already Exist!! </div>";  
+      }
+    ?>
+</body>
+</html>
