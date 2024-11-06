90d76e34e740eb92a6d4de4d1ddc17dbecc3b319
Suniksha <sunikshabenpatel@gmail.com>
2024-11-06 09:21:18 +0530

    Day-17

--- Day-7 Signup and Login Registration Form using PHP/index.php
+++ Day-7 Signup and Login Registration Form using PHP/index.php
@@ -12,7 +12,7 @@
         <div class="row">
             <div class="col-md-6 col-xm-12">
                 <h1 class="text-center">Sign up</h1>
-                <form>
+                <form action="signup.php" method="post">
                     <div class="mb-3">
                         <label for="username" class="form-label">Username</label>
                         <input type="text" class="form-control" id="username" placeholder="Enter username" autocomplete="off" name="username">
@@ -25,12 +25,12 @@
                         <label for="cpassword" class="form-label">Confirm password</label>
                         <input type="password" class="form-control" id="cpassword" placeholder="Enter Confirm password" autocomplete="off" name="cpassword">
                     </div>
-                    <button type="submit" class="btn btn-success w-100 my-3">Sign up</button>
+                    <button type="submit" class="btn btn-success w-100 my-3" name="signup">Sign up</button>
                 </form>
             </div>
             <div class="col-md-6 col-xm-12 my-5">
                 <h1 class="text-center">Login</h1>
-                <form>
+                <form action="login.php" method="post">
                     <div class="mb-3">
                         <label for="username" class="form-label">Username</label>
                         <input type="text" class="form-control" id="username" placeholder="Enter username" autocomplete="off" name="username">
@@ -39,7 +39,7 @@
                         <label for="password" class="form-label">Password</label>
                         <input type="password" class="form-control" id="password" placeholder="Enter password" autocomplete="off" name="password">
                     </div>
-                    <button type="submit" class="btn btn-dark w-100 my-3">Login up</button>
+                    <button type="submit" class="btn btn-dark w-100 my-3" name="login">Login up</button>
                 </form>
             </div>
         </div>
