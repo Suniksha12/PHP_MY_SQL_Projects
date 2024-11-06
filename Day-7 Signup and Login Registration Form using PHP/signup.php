cf307bf3726b74c6719d5441acc1229838d63468
Suniksha <sunikshabenpatel@gmail.com>
2024-11-07 01:17:07 +0530

    Day-18

--- Day-7 Signup and Login Registration Form using PHP/signup.php
+++ Day-7 Signup and Login Registration Form using PHP/signup.php
@@ -7,13 +7,29 @@
         $password = $_POST['password'];
         $cpassword = $_POST['cpassword'];
 
-        $sql = "INSERT INTO `rdata` (username,password) values('$username','$password')";
-        $result = mysqli_query($con,$sql);
-        if($result){
-            echo "Data inserted successfully";
+        // $sql = "INSERT INTO `rdata` (username,password) values('$username','$password')";
+        // $result = mysqli_query($con,$sql);
+        // if($result){
+        //     echo "Data inserted successfully";
+        // } else {
+        //     die(mysqli_error($con));
+        // }
+       $sql = "SELECT * FROM `data` WHERE username='$username'";
+       $result = mysqli_query($con,$sql);
+       if($result){
+        $num = mysqli_num_rows($result);
+        if($num>0){
+            echo "User already exist";
         } else {
-            die(mysqli_error($con));
+            $sql = "INSERT INTO `data` (username,password) values('$username','$password')";
+            $result = mysqli_query($con,$sql);
+            if($result){
+                echo "Signup successfully";
+            } else {
+                die(mysqli_error($con));
+            }
         }
+       }
     }
   }
 ?>
