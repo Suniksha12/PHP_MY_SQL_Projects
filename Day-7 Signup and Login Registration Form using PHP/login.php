b942aee26068f5c22fff0b3443392f1a800ec914
Suniksha <sunikshabenpatel@gmail.com>
2024-11-08 00:52:08 +0530

    Day-19

--- Day-7 Signup and Login Registration Form using PHP/login.php
+++ Day-7 Signup and Login Registration Form using PHP/login.php
@@ -6,6 +6,7 @@
         $password = $_POST['password'];
 
         $sql = "SELECT * FROM `rdata` WHERE username='$username' and password='$password'";
+        $result = mysqli_query($con, $sql);
         if($result){
             $num = mysqli_num_rows($result);
             if($num>0){
