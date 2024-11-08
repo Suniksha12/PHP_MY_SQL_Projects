89e9ea6f7c95db88a927c62ca6270f388761e1e8
Suniksha <sunikshabenpatel@gmail.com>
2024-11-09 00:53:13 +0530

    Day-20

--- Day-7 Signup and Login Registration Form using PHP/login.php
+++ Day-7 Signup and Login Registration Form using PHP/login.php
@@ -1,4 +1,5 @@
-<?php 
+<?php
+    $login = 0; 
    if($_SERVER['REQUEST_METHOD']=='POST'){
     include 'connect.php';
     if(isset($_POST['login'])){
@@ -10,7 +11,8 @@
         if($result){
             $num = mysqli_num_rows($result);
             if($num>0){
-                echo "login Successfull";
+                // echo "login Successfull";
+                $login = 1;
             } else {
                 echo " invalid credentials ";
             }
@@ -18,3 +20,29 @@
     }
    }
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- styling CSS-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>Login Page</title>
+</head>
+<body>
+    <?php 
+      if($login){
+        echo "<div class='alert alert-success' role='alert'> Login Successfull </div>";  
+      } else {
+        if($success){
+            echo "<div class='alert alert-success' role='alert'> Signup Successfully!! </div>";
+        }
+        if($match){
+            echo "<div class='alert alert-info' role='alert'> Password did'nt match!! </div>";
+        }
+      }
+    ?>
+</body>
+</html>
