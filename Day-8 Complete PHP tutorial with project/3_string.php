02ce38cf9900497a78cddc341e6be0a471f28511
Suniksha <sunikshabenpatel@gmail.com>
2024-11-11 01:50:48 +0530

    Day-22

--- Day-8 Complete PHP tutorial with project/3_string.php
+++ Day-8 Complete PHP tutorial with project/3_string.php
@@ -5,14 +5,24 @@
   $language="Hypertext Preprocessor";
   $len = strlen($language);
   echo $len;
+  echo "<br>";
 
   //string word counting
   $language="Hypertext Preprocessor";
   $len = str_word_count($language);
   echo $len;
+  echo "<br>";
 
   //string reverse
   $language="Hypertext Preprocessor";
   $reverse = strrev($language);
   echo $reverse;
+  echo "<br>";
+
+  //strpos
+  $language="Hypertext Preprocessor";
+  $len = strpos($language,"Preprocessor");
+  echo $len;
+  echo "<br>";
+
 ?>
