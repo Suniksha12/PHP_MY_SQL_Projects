35676dfa83d36e1641634bb60bb973df3adaed89
Suniksha <sunikshabenpatel@gmail.com>
2024-11-11 01:13:09 +0530

    Day-22

--- Day-8 Complete PHP tutorial with project/2_array.php
+++ Day-8 Complete PHP tutorial with project/2_array.php
@@ -1,6 +1,20 @@
 <?php
   //Arrays
   $colors = array("blue","green","yellow");
-  echo $colors[0];
+  echo $colors[0] . "<br>";
   
+  //echo $colors;
+  print_r($colors) . "<br>";
+
+  //indexed array
+  $colors = array("blue","green","yellow");
+  $colorlength = count($colors);
+  echo $colorlength;
+
+  for($x=0;$x<$colorlength;$x++){
+    echo $colors[$x] . "<br>";
+
+    $subject = array("suniksha"=>"cse","riya"=>"mtech");
+    echo $subject["suniksha"];
+  }
 ?>
