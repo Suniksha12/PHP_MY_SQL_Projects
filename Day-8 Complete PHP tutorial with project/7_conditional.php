835ccd1b590e6ec8aeae6ae0eb1051aa487da8a5
Suniksha <sunikshabenpatel@gmail.com>
2024-11-13 01:26:27 +0530

    Day-24

---
+++ Day-8 Complete PHP tutorial with project/7_conditional.php
@@ -0,0 +1,33 @@
+<?php
+   //conditional statements
+   //if statements
+   $a=9;
+   if($a==9){
+    echo "Yes i m 9";
+    echo "<br>";
+   }
+
+   $a=9;
+   if($a==90){
+    echo "Yes i m 9";
+    echo "<br>";
+   }
+   echo "I m outside";
+   echo "<br>";
+
+   //if-eslse statemnt
+   $day = "sunday";
+   if($day == "sunday"){
+    echo "its holiday";
+    echo "<br>";
+   }
+   else {
+    echo "Its not holiday<br>";
+    echo "<br>";
+   }
+   echo "I am optional and always printed<br>";
+   echo "<br>";
+
+   //if else ladder
+   
+?>
