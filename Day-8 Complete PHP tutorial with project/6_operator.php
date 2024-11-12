33c1c604b65e2aadc6b2e1a9dee160cca1b8e30f
Suniksha <sunikshabenpatel@gmail.com>
2024-11-12 12:58:39 +0530

    Day-23

---
+++ Day-8 Complete PHP tutorial with project/6_operator.php
@@ -0,0 +1,62 @@
+<?php
+  //operators 
+
+  //arithmetic operators
+  $x = 10;
+  $y = 20;
+  $c = $x+$y;
+  echo $c;
+  echo "<br>";
+
+  $c = $x-$y;
+  echo $c;
+  echo "<br>";
+
+  $c = $x*$y;
+  echo $c;
+  echo "<br>";
+
+  $c = $x/$y; //quotient as output
+  echo $c;
+  echo "<br>";
+
+  $c = $x%$y; //remainder as output
+  echo $c;
+  echo "<br>";
+
+  echo $x**$y;
+  echo "<br>";
+
+  //asignment operator
+  $mydata = "Cse";
+  $number = 2;
+  $number += 20;
+  echo $number;
+  echo "<br>";
+
+  $mydata = "Cse";
+  $number = 2;
+  $number -= 20;
+  echo $number;
+  echo "<br>";
+
+  $mydata = "Cse";
+  $number = 2;
+  $number *= 20;
+  echo $number;
+  echo "<br>";
+
+  $mydata = "Cse";
+  $number = 2;
+  $number /= 20;
+  echo $number;
+  echo "<br>";
+
+  $mydata = "Cse";
+  $number = 2;
+  $number %= 20;
+  echo $number;
+  echo "<br>";
+
+
+?>
