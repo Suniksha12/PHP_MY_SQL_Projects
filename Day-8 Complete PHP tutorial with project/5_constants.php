31524128677e24086726fc9ba684be7b0c48fa36
Suniksha <sunikshabenpatel@gmail.com>
2024-11-12 12:12:12 +0530

    Day-23

---
+++ Day-8 Complete PHP tutorial with project/5_constants.php
@@ -0,0 +1,19 @@
+<?php
+ //syntax of constants
+ //define(name,value,case_insensitive);
+
+    define("NAME","Suniksha");
+    echo NAME;
+    echo "<br>";
+
+//  define("_NAME","Suniksha",true);
+//  echo _Name;
+
+    define("colors",["blue","green","yellow"]);
+    echo colors[2];
+    echo "<br>";
+
+    function myColor(){
+        
+    }
+?>
