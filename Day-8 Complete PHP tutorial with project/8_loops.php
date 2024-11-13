ad02a484a45b2513c3c45f1ef021271b5a7a549e
Suniksha <sunikshabenpatel@gmail.com>
2024-11-13 13:39:23 +0530

    Day-24

---
+++ Day-8 Complete PHP tutorial with project/8_loops.php
@@ -0,0 +1,35 @@
+<?php
+  //php loops
+  //for loop
+  for($x=0;$x<10;$x++){
+    echo $x;
+    echo "<br>";
+  }
+
+  //for each
+  $fruits=array("apple","orange","banana");
+  foreach($fruits as $values){
+    echo $values . "<br>";
+  }
+
+  $mydata=array("name"=>"suniksha","branch"=>"cse");
+  foreach($mydata as $x=>$values){
+     echo "$x=>$values"."<br>";
+  }
+
+  //while loop
+  $x=1;
+  while($x<6){
+    echo $x;
+    echo "<br>";
+    $x++;
+  }
+
+  //dowhile loop
+  $x=1;
+  do{
+    echo $x;
+    echo "<br>";
+    $x++;
+  }while($x<6)
+?>
