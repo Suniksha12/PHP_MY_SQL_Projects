9ddebc71e1cbb5da2afca3a4330ee90bc837e55c
Suniksha <sunikshabenpatel@gmail.com>
2024-11-13 14:18:54 +0530

    Day-24

--- Day-8 Complete PHP tutorial with project/11_forms.php
+++ Day-8 Complete PHP tutorial with project/11_forms.php
@@ -1,6 +1,3 @@
-<?php
-  
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,7 +6,24 @@
     <title>Forms</title>
 </head>
 <body>
-    
+    <form action="<?php $_SERVER['PHP_SELF']?>" method="post">
+        <input type="text" name="fname" placeholder="first name" autocomplete="off">
+        <br>
+        <br>
+        <input type="text" name="lname" placeholder="Last name" autocomplete="off">
+        <br>
+        <br>
+        <button type="submit">Submit</button>
+
+        <?php
+          if($_SERVER['REQUEST_METHOD']=='POST'){
+            $fname = $_POST['fname'];
+            $lname = $_POST['lname'];
+            echo $fname;
+            echo $lname;
+          }
+        ?>
+    </form>
     
 </body>
 </html>
