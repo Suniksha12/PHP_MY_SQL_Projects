e76d4a72b1dca0e6724ddfcaa027361a3f8e4fba
Suniksha <sunikshabenpatel@gmail.com>
2024-11-14 09:19:31 +0530

    Day-24

---
+++ Day-8 Complete PHP tutorial with project/home.php
@@ -0,0 +1,30 @@
+<?php
+   
+   echo "Inculde files";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP include files</title>
+</head>
+<body>
+    <header>
+        <h1>Header section</h1>
+        <ul>
+            <!-- <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+            <li>Services</li>
+            <li>Features</li> -->
+            <li><a href="home.php">Home</a></li>
+            <li><a href="about.php">about</a></li>
+            <li><a href="contact.php">Contact</a></li>
+            <li><a href="services.php">Services</a></li>
+            <li><a href="features.php">features</a></li>
+        </ul>
+    </header>
+</body>
+</html>
