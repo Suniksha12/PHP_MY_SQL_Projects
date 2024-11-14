5d9da659f8252aed67e9d0504ac12dfdd37c9a19
Suniksha <sunikshabenpatel@gmail.com>
2024-11-14 10:01:23 +0530

    Day-24

--- Day-8 Complete PHP tutorial with project/13_filehandling.php
+++ Day-8 Complete PHP tutorial with project/13_filehandling.php
@@ -1,10 +1,54 @@
 <?php
 
    //read file
-   echo readfile('data.txt');
+//    echo readfile('data.txt');
 
-//    fopen()
-    $mydata = fopen("data.txt","r");
-    echo fread($mydata,filesize("data.txt"));
-    fclose($mydata);
+// //    fopen()
+//     $mydata = fopen("data.txt","r");
+//     // echo fread($mydata,filesize("data.txt"));
+//     // fclose($mydata);
+
+//     //fgets() first line from data file
+//     // echo fgets($mydata);
+//     //fclose($mydata);
+
+//     //end of file fucntion
+//     // if ($mydata) {
+//     //     // Read the file line by line
+//     //     while (!feof($mydata)) {
+//     //         echo fgets($mydata);
+//     //         echo "<br>";
+//     //     }
+//     //     // Close the file after reading
+//     //     fclose($mydata);
+//     // } else {
+//     //     // Handle the error if the file could not be opened
+//     //     echo "Error: Unable to open the file.";
+//     // }
+
+//     //fgetc character by character output
+//     if ($mydata) {
+//         // Read the file line by line
+//         while (!feof($mydata)) {
+//             echo fgetc($mydata);
+//             echo "<br>";
+//         }
+//         // Close the file after reading
+//         fclose($mydata);
+//     } else {
+//         // Handle the error if the file could not be opened
+//         echo "Error: Unable to open the file.";
+//     }
+
+    //how to write a file and create a file
+    //fopen
+
+    $mynewFile = fopen("php.txt","w");
+    echo $mynewFile;
+
+    $text = "Suniksha is a human...
+    She is a good girl.";
+    fwrite($mynewFile,$text);
+    $text = " step by step ..hasgyagvcjgtycv";
+    fwrite($mynewFile,$text);
 ?>
