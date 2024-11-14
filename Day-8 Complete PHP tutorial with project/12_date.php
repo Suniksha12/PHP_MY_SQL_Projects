5600431a748d6844b0edc621cf165e27ddaa6cb6
Suniksha <sunikshabenpatel@gmail.com>
2024-11-14 09:11:49 +0530

    Day-24

--- Day-8 Complete PHP tutorial with project/12_date.php
+++ Day-8 Complete PHP tutorial with project/12_date.php
@@ -4,5 +4,10 @@
    echo date("Y-m-d");
    echo "<br>";
    echo date("Y.m.d");
+   echo "<br>";
    echo date("l");
+   echo "<br>";
+   echo date("h:i:sa");
 ?>
+
+<p>Copyright 2010-<?php echo date("Y") ?></p>
