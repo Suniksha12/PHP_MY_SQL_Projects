76e16ff5ca62d2c912a1c03ff455006875b714aa
Suniksha <sunikshabenpatel@gmail.com>
2024-11-15 14:16:39 +0530

    Day-25

---
+++ Day-8 Complete PHP tutorial with project/information.php
@@ -0,0 +1,11 @@
+<?php
+   session_start();
+
+   if(isset($_SESSION['username'])){
+   echo "Welcome ".$_SESSION['username'];
+   echo "<br>";
+   echo "and your password is ".$_SESSION['password'];
+   } else {
+      echo "Please login again to continue";
+   }
+?>
