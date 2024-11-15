b2cf63ac398c84e66eabaea743ecb5e02083e07a
Suniksha <sunikshabenpatel@gmail.com>
2024-11-16 01:18:16 +0530

    Day-26

--- Day-8 Complete PHP tutorial with project/index1.php
+++ Day-8 Complete PHP tutorial with project/index1.php
@@ -19,16 +19,33 @@
 
     <div class="container">
         <div class="row">
-            <div class="col-md-4 col-sm-6-col-xm-12 mb-5">
-                <div class="card" style="width: 18rem;">
-                    <img src="..." class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
-                    </div>
-                </div>
-            </div>
+            <?php 
+               $sql = 'SELECT * FROM `clothes`';
+               $result = mysqli_query($con,$sql);
+               if($result) {
+                // $row = mysqli_fetch_assoc($result);
+                // echo $row['category_name'];
+                while($row = mysqli_fetch_assoc($result)){
+                     $category_id = $row['category_id'];
+                     $category_name = $row['category_name'];
+                     $category_description = $row['category_desc'];
+                     $category_image = $row['category_image'];
+                     $category_price = $row['category_price'];
+
+                     echo '<div class="col-md-4 col-sm-6-col-xm-12 mb-5">
+                             <div class="card" style="width: 18rem;">
+                                 <img src="./images/Cloth1.avif" class="card-img-top" alt="...">
+                                 <div class="card-body">
+                                     <h5 class="card-title">Card title</h5>
+                                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                     <a href="#" class="btn btn-primary">Go somewhere</a>
+                                 </div>
+                             </div>
+                         </div>';
+
+               }
+            }
+            ?>
         </div>
     </div>
 </body>
