40a89fd63a73a3c06d3edff62edbac5e0dbdfd1e
Suniksha <sunikshabenpatel@gmail.com>
2024-11-20 00:57:26 +0530

    Day-29

--- Day-10 PHP advance CRUD - Create ,Read ,Update Delete along with Pagination and Search/index.php
+++ Day-10 PHP advance CRUD - Create ,Read ,Update Delete along with Pagination and Search/index.php
@@ -45,16 +45,47 @@
                     <form id="addform" method="post" enctype="multipart/form-data">
                         <div class="modal-body">
                           <div class="form-group">
+                            <!--username-->
                             <label>Name: </label>
                                <div class="input-group">
                                    <div class="input-group-prepend">
                                        <span class="input-group-text bg-dark"><i class="bi bi-person text-light"></i></span>
                                    </div>
-                                   <input type="text" class="form-control" placeholder="Enter Your Username" autocomplete="off" required="required" id="username">
+                                   <input type="text" class="form-control" placeholder="Enter Your Username" autocomplete="off" required="required" id="username" name="username">
                                </div>
                               </div>
 
                               <!--email-->
+                              <div class="form-group">
+                                <label>Email: </label>
+                                   <div class="input-group">
+                                       <div class="input-group-prepend">
+                                           <span class="input-group-text bg-dark"><i class="bi bi-envelope text-light"></i></i></span>
+                                       </div>
+                                    <input type="email" class="form-control" placeholder="Enter Your Email" autocomplete="off" required="required" id="email" name="email">
+                                 </div>
+                                </div>
+
+                                <!--mobile-->
+                                <div class="form-group">
+                                <label>Mobile: </label>
+                                   <div class="input-group">
+                                       <div class="input-group-prepend">
+                                           <span class="input-group-text bg-dark"><i class="bi bi-telephone text-light"></i></i></span>
+                                       </div>
+                                    <input type="text" class="form-control" placeholder="Enter Your Phone no" autocomplete="off" required="required" id="mobile" name="mobile" maxlength="10" minlength="10">
+                                 </div>
+                                </div>
+
+                                <!--photo-->
+                                <div class="form-group">
+                                    <label>Photo:</label>
+                                    <div class="input-group">
+                                        <label class="custom-file-label" for="userphoto">Choose File</label>
+                                        <input type="file" class="custom-file-input" name="photo" id="userphoto" required>
+                                    </div>
+                                </div>
+                                </div>
                             </div>
                         <div class="modal-footer">
                             <button type="button" class="btn btn-dark">Submit</button>
