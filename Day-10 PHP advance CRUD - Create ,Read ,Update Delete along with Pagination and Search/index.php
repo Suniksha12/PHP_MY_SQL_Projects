863abf4814c4bb58703fdd93d2f4fc2059f0b83b
Suniksha <sunikshabenpatel@gmail.com>
2024-11-20 00:27:59 +0530

    Day-29

--- Day-10 PHP advance CRUD - Create ,Read ,Update Delete along with Pagination and Search/index.php
+++ Day-10 PHP advance CRUD - Create ,Read ,Update Delete along with Pagination and Search/index.php
@@ -1,39 +1,64 @@
 <!doctype html>
 <html lang="en">
-  <head>
+
+<head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>PHP Advance Crud</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="style.css" class="css">
     <!-- font awesome bootstrap Link -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-  </head>
-  <body>
-    
+</head>
+
+<body>
+
     <h1 class="bg-dark text-light text-center py-2">PHP Advance Crud</h1>
 
     <div class="container">
-    <div class="row">
-        <div class="col-10">
-            <div class="input-group">
-                <div class="input-group-prepend">
-                    <span class="input-group-text bg-dark"><i class="fas fa-search text-white"></i></span>
+        <!--Input search and button modal -->
+        <div class="row">
+            <div class="col-10">
+                <div class="input-group">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text bg-dark"><i class="bi bi-search text-light"></i></span>
+                    </div>
+                    <input type="text" class="form-control" placeholder="Search user..">
                 </div>
-                <input type="text" class="form-control" placeholder="Search user..">
+            </div>
+            <div class="col-2">
+                <button type="button" class="btn btn-dark" data-bs-toggle="modal" data-bs-target="#usermodal">
+                   Add New User
+                </button>
             </div>
         </div>
-        <div class="col-2">
-            <button class="btn btn-dark">
-                Add New User
-            </button>
+
+        <!-- Form Modal -->
+        <div class="modal fade" id="usermodal" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="exampleModalLabel">Adding or Updating Users</h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                       <form id="addform" method="post"></form>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary">Save changes</button>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
-    </div>
-    
+
     <!-- Jquery -->
     <script src="https://code.jquery.com/jquery-3.7.1.slim.min.js" integrity="sha256-kmHvs0B+OpCW5GVHUNjv9rOmY0IvSIRcf7zGUDTDQM8=" crossorigin="anonymous"></script>
     <!-- bootstrap Js and popper-->
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
-  </body>
+</body>
+
 </html>
