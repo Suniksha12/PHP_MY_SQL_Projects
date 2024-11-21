656ab3ac9a26da7e165ebc544a3f84af7a80fba7
Suniksha <sunikshabenpatel@gmail.com>
2024-11-21 10:40:42 +0530

    Day-30

--- Day-10 PHP advance CRUD - Create ,Read ,Update Delete along with Pagination and Search/partials/connect.php
+++ Day-10 PHP advance CRUD - Create ,Read ,Update Delete along with Pagination and Search/partials/connect.php
@@ -1,12 +1,23 @@
 <?php
-   class Database{
+class Database {
     private $dbserver = 'localhost';
     private $dbuser = 'root';
     private $dbpassword = '';
     private $dbname = 'signupforms';
     protected $conn;
 
-    //constructor
-    public 
-   }
+    // Constructor
+    public function __construct() {
+        try {
+            $dsn = "mysql:host={$this->dbserver};dbname={$this->dbname};charset=utf8";
+            $options = array(
+                PDO::ATTR_PERSISTENT => true, // Set persistent connection
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION // Set error mode to exception
+            );
+            $this->conn = new PDO($dsn, $this->dbuser, $this->dbpassword, $options);
+        } catch (PDOException $e) { // Corrected here
+            echo "Connection Error: " . $e->getMessage();
+        }
+    }
+}
 ?>
