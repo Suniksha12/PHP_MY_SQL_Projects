6199b7f6fd275dcfe33f73d4a94257bb3de5b484
Suniksha <sunikshabenpatel@gmail.com>
2024-11-21 10:29:42 +0530

    Day-30

---
+++ Day-10 PHP advance CRUD - Create ,Read ,Update Delete along with Pagination and Search/partials/connect.php
@@ -0,0 +1,12 @@
+<?php
+   class Database{
+    private $dbserver = 'localhost';
+    private $dbuser = 'root';
+    private $dbpassword = '';
+    private $dbname = 'signupforms';
+    protected $conn;
+
+    //constructor
+    public 
+   }
+?>
