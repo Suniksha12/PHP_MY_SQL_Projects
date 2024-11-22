616271ec5053e29e0f34b10a0ab24759e79e632c
Suniksha <sunikshabenpatel@gmail.com>
2024-11-22 21:06:06 +0530

    Day-30

--- Day-10 PHP advance CRUD - Create ,Read ,Update Delete along with Pagination and Search/index.php
+++ Day-10 PHP advance CRUD - Create ,Read ,Update Delete along with Pagination and Search/index.php
@@ -41,28 +41,7 @@
         </div>
 
         <!--table-->
-        <table class="table" id="usertable">
-            <thead class="table-dark">
-                <th scope="col">Image</th>
-                <th scope="col">Name</th>
-                <th scope="col">Email</th>
-                <th scope="col">Phone</th>
-                <th scope="col">Operations</th>
-            </thead>
-            <tbody>
-                <tr>
-                    <th scope="row">Picture 1</th>
-                    <td>Suniksha</td>
-                    <td>suniksha@gmail.com</td>
-                    <td>2907917825</td>
-                    <td>
-                        <span>Edit</span>
-                        <span>Profile</span>
-                        <span>Delete</span>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
+        <?php require_once 'tableData.php' ?>
 
         <!-- pagination-->
         <nav aria-label="Page navigation example" id="pagination">
---
+++ Day-10 PHP advance CRUD - Create ,Read ,Update Delete along with Pagination and Search/tableData.php
@@ -0,0 +1,22 @@
+<table class="table" id="usertable">
+    <thead class="table-dark">
+        <th scope="col">Image</th>
+        <th scope="col">Name</th>
+        <th scope="col">Email</th>
+        <th scope="col">Phone</th>
+        <th scope="col">Operations</th>
+    </thead>
+    <tbody>
+        <tr>
+            <th scope="row">Picture 1</th>
+            <td>Suniksha</td>
+            <td>suniksha@gmail.com</td>
+            <td>2907917825</td>
+            <td>
+                <span>Edit</span>
+                <span>Profile</span>
+                <span>Delete</span>
+            </td>
+        </tr>
+    </tbody>
+</table>
