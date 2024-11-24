42894afea6f4a9e6e9f8110fd32c8348f74930a8
Suniksha <sunikshabenpatel@gmail.com>
2024-11-24 18:53:08 +0530

    Day-31

--- Day-10 PHP advance CRUD - Create ,Read ,Update Delete along with Pagination and Search/partials/User.php
+++ Day-10 PHP advance CRUD - Create ,Read ,Update Delete along with Pagination and Search/partials/User.php
@@ -67,6 +67,22 @@
         }
 
         //function to uplaod photo
+        public function uploadPhoto($file){
+            if(!empty($file)){
+               $filrTempPath = $file['tmp_name'];
+               $fileName = $file['name'];
+               $fileType= $file['type'];
+               $fileNameCmps = explode('.',$fileName);
+               $fileExtension = strtolower(end($fileNameCmps));
+               $newFileName = md5(time().$fileName). '.' .$fileExtension;
+               $allowedExtn = ["png","jpg","jpeg"];
+               if(in_array($fileExtension,$allowedExtn)){
+                $uploadFileDir = getcwd().'/uploads/';
+                $destFilePath = $uploadFileDir.$newFileName;
+                if(move_uploaded_file())
+               }
+            }
+        }
 
         //function to update
 
