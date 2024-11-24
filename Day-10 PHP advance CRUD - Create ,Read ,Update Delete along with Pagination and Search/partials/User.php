af98ebff3ec01476f7c58e0ccdaaa03650b47988
Suniksha <sunikshabenpatel@gmail.com>
2024-11-24 13:43:45 +0530

    Day-31

--- Day-10 PHP advance CRUD - Create ,Read ,Update Delete along with Pagination and Search/partials/User.php
+++ Day-10 PHP advance CRUD - Create ,Read ,Update Delete along with Pagination and Search/partials/User.php
@@ -5,6 +5,31 @@
    class User extends Database{
         protected $tableName="usertable";
 
-        
+        //function to add users
+        public function add($data){
+            if(!empty($data)){
+                $fields = $placeholder=[];
+                foreach($data as $fields => $value){
+                    $fields[] = $field;
+                    $placeholder[] = ":{field}";
+                }
+            }
+            $sql = "INSERT INTO {$this->tableName} (pname,email,phone) VALUES (:pname,:email,:phone);"
+        }
+
+        //function to get rows
+
+        //function to get single ow
+
+        //function to count number of rows
+
+        //function to uplaod photo
+
+        //function to update
+
+        //function to delete
+
+        //function for search
+
    }
 ?>
