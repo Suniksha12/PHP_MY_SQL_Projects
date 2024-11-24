dcbb1f4a1bfc8aee30d81123cfbb07da2564e51e
Suniksha <sunikshabenpatel@gmail.com>
2024-11-24 14:01:42 +0530

    Day-31

--- Day-10 PHP advance CRUD - Create ,Read ,Update Delete along with Pagination and Search/partials/User.php
+++ Day-10 PHP advance CRUD - Create ,Read ,Update Delete along with Pagination and Search/partials/User.php
@@ -30,8 +30,20 @@
         }
 
         //function to get rows
+        public function getRows($start=0,$limit=4){
+            $sql = "SELECT * FROM {$this->tableName} ORDER BY DESC LIMIT {$start},{$limit}";
+            $stmt = $this->conn->prepare($sql);
+            $stmt->execute();
+            if($stmt->rowCount()>0){
+                $results=$stmt->fetchAll(PDO::FETCH_ASSOC);
+                
+            } else {
+                $results = [];
+            }
+            return $results;
+        }
 
-        //function to get single ow
+        //function to get single row
 
         //function to count number of rows
 
