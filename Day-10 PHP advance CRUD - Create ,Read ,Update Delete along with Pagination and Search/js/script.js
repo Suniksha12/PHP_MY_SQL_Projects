138d7d1f30e64f239ea19935507a784d147b30b9
Suniksha <sunikshabenpatel@gmail.com>
2024-11-25 13:52:27 +0530

    Day-32

--- Day-10 PHP advance CRUD - Create ,Read ,Update Delete along with Pagination and Search/js/script.js
+++ Day-10 PHP advance CRUD - Create ,Read ,Update Delete along with Pagination and Search/js/script.js
@@ -5,7 +5,22 @@ $(document).ready(function(){
         e.preventDefault();
         //ajax
         $.ajax({
-            url:""
+            url:"/PHP_My_SQL_Projects/Day-10 PHP advance CRUD - Create ,Read ,Update Delete along with Pagination and Search/ajax.php",
+            type:"POST",
+            dataType: "json",
+            data: new FormData(this),
+            processData:false,
+            contentType:false,
+            beforeSending:function(){
+                console.log("Wait...Data is loading");
+            },
+            success:function(response){
+                console.log(response);
+            },
+            error:function(request,error){
+                console.log(arguments);
+                console.log("Error"+error);
+            },           
         })
     })
 })
