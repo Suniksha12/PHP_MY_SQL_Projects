a657d4456aadb033c6998cbbb7a58258ab36f762
Suniksha <sunikshabenpatel@gmail.com>
2024-11-25 14:55:02 +0530

    Day-32

--- Day-10 PHP advance CRUD - Create ,Read ,Update Delete along with Pagination and Search/ajax.php
+++ Day-10 PHP advance CRUD - Create ,Read ,Update Delete along with Pagination and Search/ajax.php
@@ -13,5 +13,31 @@
         $pname = $_POST['username'];
         $email = $_POST['email'];
         $mobile = $_POST['mobile'];
-        $photo 
+        $photo = $_POST['photo'];
+
+        $playerid = (!empty($_POST['userId']))? $_POST['userId']: "";
+        
+        $imagename = "";
+        if(!empty($photo['name'])){
+            $imagename = $obj->uploadPhoto($photo);
+            $playerData = [
+                'pname'=>$pname,
+                'email'=>$email,
+                'mobile'=>$mobile,
+                'photo'=>$imagename,
+            ];
+        } else {
+            $playerData = [
+                'pname'=>$pname,
+                'email'=>$email,
+                'mobile'=>$mobile,
+            ];
+        }
+        $playerid=$obj->add($playerData);
+        if(!empty($playerid)){
+            $player=$obj->getRow('id',$playerid);
+            echo json_encode($player);
+            exit();
+        }
+     }
 ?>
