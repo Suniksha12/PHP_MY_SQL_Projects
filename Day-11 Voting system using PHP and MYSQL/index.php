16de55accafce88574ce4d820ae074ed7b1b582f
Suniksha <sunikshabenpatel@gmail.com>
2024-12-02 09:56:49 +0530

    Day-39 Voting form building up

--- Day-11 Voting system using PHP and MYSQL/index.php
+++ Day-11 Voting system using PHP and MYSQL/index.php
@@ -9,25 +9,27 @@
 </head>
 <body class="bg-dark">
     <h1 class="text-info text-center p-3">Voting System</h1>
-    <div class="bg-info">
+    <div class="bg-info py-4">
          <h2 class="text-center">Login</h2>
-         <div class="container">
+         <div class="container text-center ">
             <form action="">
                 <div class="mb-3">
                     <input type="text" class="form-control w-50 m-auto" name="username" placeholder="Enter your username" required>
                 </div>
                 <div class="mb-3">
-                    <input type="text" class="form-control w-50 m-auto" name="mobile" placeholder="Enter your mobile" required maxlength="10" minlength="10">
+                    <input type="text" class="form-control w-50 m-auto" name="mobile" placeholder="Enter your mobile Number" required maxlength="10" minlength="10">
                 </div>
                 <div class="mb-3">
                     <input type="password" class="form-control w-50 m-auto" name="mobile" placeholder="Enter your password" required>
                 </div>
                 <div class="mb-3">
-                    <select name="std" class=" form-control w-50 m-auto">
+                    <select name="std" class=" form-select w-50 m-auto">
                         <option value="group">Group</option>
                         <option value="voter">Voter</option>
                     </select>
                 </div>
+                <button type="submit" class="btn btn-dark my-4 ">Login</button>
+                <p>Don't have and account? <a href="#" class="text-white"> Register Here</a></p>
             </form>
          </div>
     </div>
