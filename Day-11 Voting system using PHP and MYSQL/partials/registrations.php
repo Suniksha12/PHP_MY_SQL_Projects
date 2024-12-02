86fc829acf29b282a189bbb9e86b9a886390fbeb
Suniksha <sunikshabenpatel@gmail.com>
2024-12-02 10:04:15 +0530

    Day-39 building registration page

--- Day-11 Voting system using PHP and MYSQL/partials/registrations.php
+++ Day-11 Voting system using PHP and MYSQL/partials/registrations.php
@@ -9,6 +9,16 @@
 </head>
 <body class="bg-dark">
     <h1 class="text-info text-center p-3">Voting System</h1>
+    <div class="bg-info py-4">
+        <h2 class="text-center">Register Account</h2>
+        <div class="container">
+            <form action="">
+                <div class="mb-3">
+                    <input type="text" class="form-control w-50 m-auto" placeholder="Enter your username" required name="username">
+                </div>
+            </form>
+        </div>
+    </div>
 
     <!-- script js -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
