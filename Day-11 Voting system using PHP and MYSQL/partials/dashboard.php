09fddf29239e6f2f1424fb05cd545d8455ed4c1e
Suniksha <sunikshabenpatel@gmail.com>
2024-12-04 00:45:21 +0530

    Day-41 craeting dashboard.php

--- Day-11 Voting system using PHP and MYSQL/partials/dashboard.php
+++ Day-11 Voting system using PHP and MYSQL/partials/dashboard.php
@@ -3,9 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dashboard</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="../Day-11 Voting system using PHP and MYSQL/css/style.css">
+    <title>PHP Voting System</title>
 </head>
-<body>
-    <h3>Hey!</h3>
+<body class="bg-secondary text-light">
+    <div class="container my-5">
+        <a href="../"><button class="btn btn-dark text-light px-3">Back</button></a>
+        <a href="logout.php"><button class="btn btn-dark text-light px-3">Log Out</button></a>
+
+    </div>
+ <!-- script js -->
+ <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
