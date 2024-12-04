bef8666580bef5f2a63a5ec94ffb399d0837b68c
Suniksha <sunikshabenpatel@gmail.com>
2024-12-04 09:30:52 +0530

    Day-41 frontend part completed for dashboard.php

--- Day-11 Voting system using PHP and MYSQL/partials/dashboard.php
+++ Day-11 Voting system using PHP and MYSQL/partials/dashboard.php
@@ -11,7 +11,31 @@
     <div class="container my-5">
         <a href="../"><button class="btn btn-dark text-light px-3">Back</button></a>
         <a href="logout.php"><button class="btn btn-dark text-light px-3">Log Out</button></a>
-
+        <h1 class="my-3">Voting System</h1>
+        <div class="row my-5">
+           <div class="col-md-7">
+            <!--groups-->
+            <div class="row">
+                <div class="col-md-4">
+                    <img src="" alt="Image1">
+                </div>
+                <div class="col-md-8">
+                    <strong class="text-dark h5">Group name:</strong><br>
+                    <strong class="text-dark h5">Votes:</strong><br>
+                </div>
+            </div>
+            <hr>
+           </div>
+           <div class="col md 5">
+               <!--User Profile-->
+               <img src="" alt="User Image">
+               <br>
+               <br>
+               <strong class="text-dark h5">Name:</strong><br><br>
+               <strong class="text-dark h5">Mobile:</strong><br><br>
+               <strong class="text-dark h5">Status:</strong><br><br>
+           </div>
+        </div>
     </div>
  <!-- script js -->
  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
