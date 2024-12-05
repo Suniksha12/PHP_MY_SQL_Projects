4f169e36a5c5d1ff46e4676826e5470fb833f4aa
Suniksha <sunikshabenpatel@gmail.com>
2024-12-05 13:10:26 +0530

    Day-42 completed voting.php

--- Day-11 Voting system using PHP and MYSQL/partials/voting.php
+++ Day-11 Voting system using PHP and MYSQL/partials/voting.php
@@ -9,6 +9,22 @@
 
    $updatevotes=mysqli_query($con,"update `userdata` set votes='$totalvotes' where id='$gid' ");
 
-   $updatevotes=mysqli_query($con,"update `userdata` set status = 1 where id='$uid'");
+   $updatestatus=mysqli_query($con,"update `userdata` set status = 1 where id='$uid'");
+
+   if($updatevotes and $updatestatus){
+          $getgroups = mysqli_query($con,"SELECT username,photo,votes,id from `userdata` where standard='groups'");
+          $groups=mysqli_fetch_all($getgroups,MYSQLI_ASSOC);
+          $_SESSION['groups']=$groups;
+          $_SESSION['status']=1;
+          echo '<script>
+          alert("Technical error !! Voting successfull");
+          window.location="../partials/dashboard.php";            
+     </script>';
+    }else {
+        echo '<script>
+             alert("Technical error !! Vote after sometime");
+             window.location="../partials/dashboard.php";            
+        </script>';
+    }
 
 ?>
