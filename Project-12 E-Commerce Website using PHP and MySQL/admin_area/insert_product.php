9d98903c40250372e81f4220c9c7b4e4c5e1ff21
Suniksha <sunikshabenpatel@gmail.com>
2024-12-08 12:44:41 +0530

    Day-45 completed insert product form

--- Project-12 E-Commerce Website using PHP and MySQL/admin_area/insert_product.php
+++ Project-12 E-Commerce Website using PHP and MySQL/admin_area/insert_product.php
@@ -25,16 +25,19 @@
                 <label for="product_title" class="form-label">Product title</label>
                 <input type="text" name="product_title" id="product_title" class="form-control" placeholder="Enter Product title" autocomplete="off" required>
             </div>
+
             <!-- description -->
             <div class="form-outline mb-4 w-50 m-auto">
                 <label for="description" class="form-label">Product description</label>
                 <input type="text" name="description" id="description" class="form-control" placeholder="Enter Product description" autocomplete="off" required>
             </div>
+
             <!-- Keywords -->
             <div class="form-outline mb-4 w-50 m-auto">
                 <label for="product_keywords" class="form-label">Product Keywords</label>
                 <input type="text" name="product_keywords" id="product_keywords" class="form-control" placeholder="Enter Product keywords" autocomplete="off" required>
             </div>
+
             <!--categories-->
             <div class="form-outline mb-4 w-50 m-auto">
                 <select name="product_category" id="" class="form-select">
@@ -42,18 +45,48 @@
                     <option value="">Category1</option>
                     <option value="">Category2</option>
                     <option value="">Category3</option>
-                    <option value="">Category1</option>
+                    <option value="">Category4</option>
                 </select>
             </div>
+
             <!--Brands-->
             <div class="form-outline mb-4 w-50 m-auto">
                 <select name="product_brands" id="" class="form-select">
-                    <option value="">Select a category</option>
-                    <option value="">Category1</option>
-                    <option value="">Category2</option>
-                    <option value="">Category3</option>
-                    <option value="">Category1</option>
+                    <option value="">Select a Brands</option>
+                    <option value="">Brands1</option>
+                    <option value="">Brands2</option>
+                    <option value="">Brands3</option>
+                    <option value="">Brands4</option>
                 </select>
+            </div>
+
+            <!--Image 1-->
+            <div class="form-outline mb-4 w-50 m-auto">
+                <label for="product_image1" class="form-label">Product Image 1</label>
+                <input type="file" name="product_image1" id="product_image1" class="form-control" required>
+            </div>
+
+            <!--Image 2-->
+            <div class="form-outline mb-4 w-50 m-auto">
+                <label for="product_image2" class="form-label">Product Image 2</label>
+                <input type="file" name="product_image2" id="product_image2" class="form-control" required>
+            </div>
+
+            <!--Image 3-->
+            <div class="form-outline mb-4 w-50 m-auto">
+                <label for="product_image3" class="form-label">Product Image 3</label>
+                <input type="file" name="product_image3" id="product_image3" class="form-control" required>
+            </div>
+
+            <!-- Price -->
+            <div class="form-outline mb-4 w-50 m-auto">
+                <label for="product_price" class="form-label">Product Price</label>
+                <input type="text" name="product_price" id="product_price" class="form-control" placeholder="Enter Product Price" autocomplete="off" required>
+            </div>
+
+            <!--Button-->
+            <div class="form-outline mb-4 w-50 m-auto">
+                <input type="submit" name="insert_product" class="btn btn-info mb-3 px-3" value="Insert Products">
             </div>
         </form>
     </div>
