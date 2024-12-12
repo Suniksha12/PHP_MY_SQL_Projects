bb3bdc5f22a82aa1426c775ca9a4d16a4e20b191
Suniksha <sunikshabenpatel@gmail.com>
2024-12-12 12:29:50 +0530

    Day-49 fixing bug issues

--- Project-12 E-Commerce Website using PHP and MySQL/index.php
+++ Project-12 E-Commerce Website using PHP and MySQL/index.php
@@ -1,6 +1,7 @@
 <!-- connect file -->
 <?php
     include('includes/connect.php');
+    include('functions/common_function.php');
 ?>
 
 <!DOCTYPE html>
@@ -85,28 +86,7 @@
                 <div class="row">
                     <!--fetching products-->
                     <?php
-                       $select_query = "SELECT * FROM `products` Order by rand() LIMIT 0,9";
-                       $result_query = mysqli_query($con, $select_query);
-                       while($row=mysqli_fetch_assoc($result_query)){
-                           $product_id = $row['product_id'];
-                           $product_title = $row['product_title'];
-                           $product_description = $row['product_description'];
-                           $product_image1 = $row['product_image1'];
-                           $product_price = $row['product_price'];
-                           $category_id = $row['category_id'];
-                           $brand_id = $row['brand_id'];
-                           echo "<div class='col-md-4 mb-2'>
-                           <div class='card'>
-                            <img src='./admin_area/product_images/$product_image1' class='card-img-top' alt='$product_title'>
-                            <div class=card-body'>
-                                <h5 class='card-title'>$product_title</h5>
-                                <p class='card-text'>$product_description</p>
-                                <a href='#' class='btn btn-info'>Add to cart</a>
-                                <a href='#' class='btn btn-secondary'>View more</a>
-                                </div>
-                            </div>
-                        </div>";
-                       }
+                       getproducts();
                     ?>
                 <!--row end-->
                 </div>
