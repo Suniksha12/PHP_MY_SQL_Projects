f3ce651bf5915cfef832a53a399d44990c0cf21b
Suniksha <sunikshabenpatel@gmail.com>
2024-12-15 14:52:54 +0530

    Day-51 working on diplaying number of items in cart dynamically

--- Project-12 E-Commerce Website using PHP and MySQL/functions/common_function.php
+++ Project-12 E-Commerce Website using PHP and MySQL/functions/common_function.php
@@ -307,18 +307,16 @@
         if(isset($_GET['add_to_cart'])){
             global $con;
             $get_ip_add=getIPAddress();
-            $select_query="SELECT * FROM `card_details` WHERE ip_address='$get_ip_add' and product_id=$get_product_id";
+            $select_query="SELECT * FROM `card_details` WHERE ip_address='$get_ip_add'";
             $result_query=mysqli_query($con,$select_query);
-            $num_of_rows = mysqli_num_rows($result_query);
-            if ($num_of_rows > 0) {
-                echo "<script> alert('This item is already present inside cart')</script>";
-                echo "<script>window.open('index.php','_self')</script>";
+            $count_cart_items = mysqli_num_rows($result_query);
             }else {
-                $insert_query="INSERT INTO `card_details` (product_id,ip_address,quantity) values ($get_product_id,'$get_ip_add',0)";
-                $result_query=mysqli_query($con,$insert_query);
-                echo "<script>alert('Item is added to cart')</script>";
-                echo "<script>window.open('index.php','_self')</script>";
+                global $con;
+                $get_ip_add=getIPAddress();
+                $select_query="SELECT * FROM `card_details` WHERE ip_address='$get_ip_add'";
+                $result_query=mysqli_query($con,$select_query);
+                $count_cart_items = mysqli_num_rows($result_query);
             }
+            echo $count_cart_items;
         }
-    }
 ?>
