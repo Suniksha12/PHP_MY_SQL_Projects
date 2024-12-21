c047e6683c31481a4b3aa0b2d85b56577c63f6ce
Suniksha <sunikshabenpatel@gmail.com>
2024-12-21 15:19:27 +0530

    Day-56 completed user_registrations form.php

--- Project-12 E-Commerce Website using PHP and MySQL/users_area/user_registration.php
+++ Project-12 E-Commerce Website using PHP and MySQL/users_area/user_registration.php
@@ -63,8 +63,9 @@
                         <input type="text" id="user_contact" class="form-control" placeholder="Enter your Mobile Number" autocomplete="off" required name="user_contact">
                     </div>
 
-                    <div class="text-center">
-                        <input type="submit" value="Register" class="bg-info">
+                    <div class="mt-4 pt-2">
+                        <input type="submit" value="Register" class="bg-info py-2 px-3 border-0" name="user_register">
+                        <p class="small fw-bold mt-2 pt-1 mb-0">Already have an account ?<a href="user_login.php" class="text-danger"> Login</a></p>
                     </div>
                 </form>
             </div>
