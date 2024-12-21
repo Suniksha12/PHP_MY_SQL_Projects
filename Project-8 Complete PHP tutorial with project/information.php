1c8c009e12c66cb566af6ce1555c42f9f53fe1bf
Suniksha <sunikshabenpatel@gmail.com>
2024-12-21 13:15:04 +0530

    Day-56 understanding sessions

--- Project-8 Complete PHP tutorial with project/information.php
+++ Project-8 Complete PHP tutorial with project/information.php
@@ -1,9 +1,13 @@
 <?php
     session_start();
-    echo "Welcome".$_SESSION['username'];
-    echo "<br>";
-    echo "and your password is ".$_SESSION['password'];
-    echo "<br>";
-    echo "Your email is ".$_SESSION['email'];
+    if(isset($_SESSION['username'])){
+      echo "Welcome".$_SESSION['username'];
+      echo "<br>";
+      echo "and your password is ".$_SESSION['password'];
+      echo "<br>";
+      echo "Your email is ".$_SESSION['email'];
+    } else {
+      echo "Please login again to continue";
+    }
 
 ?>
