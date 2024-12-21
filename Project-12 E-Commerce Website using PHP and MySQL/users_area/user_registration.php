aff65dd9dc9384101097434d39786f72b5eaad18
Suniksha <sunikshabenpatel@gmail.com>
2024-12-21 15:14:04 +0530

    Day-56 creating user_registration.php

--- Project-12 E-Commerce Website using PHP and MySQL/users_area/user_registration.php
+++ Project-12 E-Commerce Website using PHP and MySQL/users_area/user_registration.php
@@ -18,7 +18,7 @@
         <h2 class="text-center">
             New User Registration
         </h2>
-        <div class="row">
+        <div class="row d-flex align-items-center justify-content-center">
             <div class="col-lg-12 col-xl-6">
                 <form action="" method="post" enctype="multipart/form-data">
                     <!--username Field-->
@@ -30,10 +30,42 @@
                     <!--Email Field-->
                     <div class="form-outline md-4">
                         <label for="user_email" class="form-label">Email</label>
-                        <input type="text" id="user_email" class="form-control" placeholder="Enter your Email" autocomplete="off" required name="user_email">
+                        <input type="email" id="user_email" class="form-control" placeholder="Enter your Email" autocomplete="off" required name="user_email">
                     </div>
 
+                    <!-- Image field-->
+                    <div class="form-outline md-4">
+                        <label for="user_image" class="form-label">User Image</label>
+                        <input type="file" id="user_image" class="form-control" required name="user_image">
+                    </div>
 
+                    <!--password Field-->
+                    <div class="form-outline md-4">
+                        <label for="user_password" class="form-label">Password</label>
+                        <input type="password" id="user_password" class="form-control" placeholder="Enter your password" autocomplete="off" required name="user_password">
+                    </div>
+
+                    <!--Confirm password Field-->
+                    <div class="form-outline md-4">
+                        <label for="conf_user_password" class="form-label">Confirm Password</label>
+                        <input type="password" id="conf_user_password" class="form-control" placeholder="Confirm Password" autocomplete="off" required name="conf_user_password">
+                    </div>
+
+                    <!-- Address Field-->
+                    <div class="form-outline md-4">
+                        <label for="user_address" class="form-label">Address</label>
+                        <input type="text" id="user_address" class="form-control" placeholder="Enter your address" autocomplete="off" required name="user_address">
+                    </div>
+
+                    <!-- Contact Field-->
+                    <div class="form-outline md-4">
+                        <label for="user_contact" class="form-label">Contact</label>
+                        <input type="text" id="user_contact" class="form-control" placeholder="Enter your Mobile Number" autocomplete="off" required name="user_contact">
+                    </div>
+
+                    <div class="text-center">
+                        <input type="submit" value="Register" class="bg-info">
+                    </div>
                 </form>
             </div>
         </div>
