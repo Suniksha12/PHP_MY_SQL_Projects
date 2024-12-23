f0be0647a91b3215444d10468a5ea8989a89553c
Suniksha <sunikshabenpatel@gmail.com>
2024-12-23 18:17:16 +0530

    Day-58 putting if else condition

--- Project-12 E-Commerce Website using PHP and MySQL/users_area/user_registration.php
+++ Project-12 E-Commerce Website using PHP and MySQL/users_area/user_registration.php
@@ -98,17 +98,19 @@
         $user_image_tmp=$_FILES['user_image']['tmp_name'];
         $user_ip=getIPAddress();
 
+        //select query
+        $select_query="SELECT * FROM `user_table` WHERE username='$user_username'";
+        $result = mysqli_query($con,$select_query);
+        $rows_count=mysqli_num_rows($result);
+        if($rows_count>0){
+            echo "<script>alert('Username already exist')</script>";
+        } else {
+
         move_uploaded_file($user_image_tmp,"./user_images/$user_image");
 
         //Insert Query
         $insert_query = "INSERT INTO `user_table` (username,user_email,user_password,user_image,user_ip,user_address,user_mobile) values('$user_username','$user_email','$user_password','$user_image','$user_ip','$user_address','$user_contact')";
         $sql_execute=mysqli_query($con,$insert_query);
-
-        if($sql_execute){
-            echo "<script>alert('Data inserted Successfully')</script>";
-        } else {
-            die(mysqli_error($con));
         }
-
     }
 ?>
