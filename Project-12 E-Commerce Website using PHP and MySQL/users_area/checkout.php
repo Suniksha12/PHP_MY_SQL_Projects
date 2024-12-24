f4b9b5a25c7dbe05c8fdb9357adfc0f0f611b16e
Suniksha <sunikshabenpatel@gmail.com>
2024-12-25 00:47:27 +0530

    Day-60 Displaying logged in username

--- Project-12 E-Commerce Website using PHP and MySQL/users_area/checkout.php
+++ Project-12 E-Commerce Website using PHP and MySQL/users_area/checkout.php
@@ -34,13 +34,13 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                         <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+                            <a class="nav-link active" aria-current="page" href="../index.php">Home</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="display_all.php">Products</a>
+                            <a class="nav-link" href="../display_all.php">Products</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Register</a>
+                            <a class="nav-link" href="user_registration.php">Register</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="#">Contact</a>
