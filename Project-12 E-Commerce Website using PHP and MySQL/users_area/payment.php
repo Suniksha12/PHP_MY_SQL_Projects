bdb10f9eb79f5bd372de982fc35849c3c557e2ef
Suniksha <sunikshabenpatel@gmail.com>
2024-12-25 19:47:15 +0530

    Day-60 building payment.php page

--- Project-12 E-Commerce Website using PHP and MySQL/users_area/payment.php
+++ Project-12 E-Commerce Website using PHP and MySQL/users_area/payment.php
@@ -1,11 +1,36 @@
+<?php
+    include_once("../includes/connect.php");
+    include_once("../functions/common_function.php");
+?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <!-- Bootstrap css Link -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <!--FonT awesome link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/brands.min.css" integrity="sha512-7rXIvd/xj1FnI2zKQjjXzDFxC4twqBByp8yxOgEQJs4C/aNzNyoQsOO7VxH0RgYNhbAYLJLwzqslaP50KTTHIQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!--Css link -->
+    <link rel="stylesheet" href="../css/style.css" class="css">
+    <title>Payment Page</title>
 </head>
+
 <body>
-    <h1>Payment</h1>
+    <div class="container">
+        <h2 class="text-center text-info">
+            Payment Options
+        </h2>
+        <div class="row">
+            <a href="http://www.paypal.com"><img src="../images/payment.jpg" alt=""></a>
+        </div>
+    </div>
+
+    <!--script.js-->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
 </body>
+
 </html>
