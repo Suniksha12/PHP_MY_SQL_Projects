1380af39e7c73509bfde4d6d03d289989e4530c9
Suniksha <sunikshabenpatel@gmail.com>
2024-12-26 21:49:19 +0530

    Day-61 writing .php code

--- Project-12 E-Commerce Website using PHP and MySQL/users_area/order.php
+++ Project-12 E-Commerce Website using PHP and MySQL/users_area/order.php
@@ -1,4 +1,15 @@
 <?php
     include_once("../includes/connect.php");
     include_once("../functions/common_function.php");
+
+    if(isset($_GET['user_id'])){
+        $user_id = $_GET['user_id'];
+
+    }
+
+    //getting total items and total price of all items
+    $get_ip_address = getIPAddress();
+    $total_price=0;
+    $cart_query_price="SELECT * FROM `card_details` WHERE ip_address='$get_ip_address'";
+    
 ?>
