74c5971afe1f65af704fd8fda90d2bbd8f1ca44f
Suniksha <sunikshabenpatel@gmail.com>
2024-12-26 10:45:09 +0530

    Day-61 working on payment page

--- Project-12 E-Commerce Website using PHP and MySQL/users_area/payment.php
+++ Project-12 E-Commerce Website using PHP and MySQL/users_area/payment.php
@@ -18,15 +18,21 @@
     <title>Payment Page</title>
 </head>
 <style>
-    img{
-        width: 75%;
+    .payment_img{
+        width: 60%;
+        height: 60%;
+        margin: auto;
+        display: block;
     }
 </style>
 <body>
     <!-- php code to access user id -->
      <?php
         $user_ip = getIPAddress();
-        $get_user = ""
+        $get_user = "SELECT * FROM `user_table`  WHERE user_ip='$user_ip'";
+        $result = mysqli_query($con, $get_user);
+        $run_query=mysqli_fetch_array($result);
+        $user_id=$run_query['user_id'];
      ?>
     <div class="container">
         <h2 class="text-center text-info">
@@ -34,10 +40,10 @@
         </h2>
         <div class="row d-flex justify-content-center align-items-center my-5">
             <div class="col-md-6">
-                <a href="http://www.paypal.com" target="_blank"><img src="../images/payment.jpg" alt=""></a>
+                <a href="http://www.paypal.com" target="_blank"><img src="../images/payment.jpg" alt="" class="payment_img"></a>
             </div>
             <div class="col-md-6">
-                <a href="order.php"><h2 class="text-center">Pay Offline</h2></a>
+                <a href="order.php?user_id=<?php  echo $user_id ?>"><h2 class="text-center">Pay Offline</h2></a>
             </div>
         </div>
     </div>
