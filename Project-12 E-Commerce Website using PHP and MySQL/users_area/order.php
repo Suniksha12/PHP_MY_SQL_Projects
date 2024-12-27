6425f7e50867e26c3ae8527a29055c9c7b512686
Suniksha <sunikshabenpatel@gmail.com>
2024-12-27 15:25:26 +0530

    Day-62 working on orders page-inserting orders

--- Project-12 E-Commerce Website using PHP and MySQL/users_area/order.php
+++ Project-12 E-Commerce Website using PHP and MySQL/users_area/order.php
@@ -51,5 +51,11 @@
     $insert_pending_orders = "INSERT INTO `orders_pending` (user_id,invoice_number,product_id,quantity,order_status)
                     values ($user_id,$invoice_number,$product_id,$quantity,'$status')";
     $result_pending_orders=mysqli_query($con,$insert_pending_orders);
-    
+
+
+    //delete items  from cart
+    $empty_cart="DELETE FROM `card_details` WHERE ip_address='$get_ip_address'";
+    $result_delete = mysqli_query($con,$empty_cart);
+
+
 ?>
