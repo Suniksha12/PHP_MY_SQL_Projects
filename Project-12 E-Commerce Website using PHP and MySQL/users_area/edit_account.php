bb49bb2a22ee9c428213d53782bb49b361689a60
Suniksha <sunikshabenpatel@gmail.com>
2024-12-28 11:26:39 +0530

    Day-63 building edit account

---
+++ Project-12 E-Commerce Website using PHP and MySQL/users_area/edit_account.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Edit account</title>
+</head>
+<body>
+    <h3 class="text-center text-success mb-4">Edit Account</h3>
+    <form action="" method="post" enctype="multipart/form-data">
+        <div class="form-outline mb-4">
+            
+        </div>
+    </form>
+</body>
+</html>
