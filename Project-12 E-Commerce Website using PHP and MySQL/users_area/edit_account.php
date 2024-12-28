0d93bc5ee79b03b5c305bdeb4781fad53e24714b
Suniksha <sunikshabenpatel@gmail.com>
2024-12-28 12:19:37 +0530

    Day-63 adding php code for submitting data to database

--- Project-12 E-Commerce Website using PHP and MySQL/users_area/edit_account.php
+++ Project-12 E-Commerce Website using PHP and MySQL/users_area/edit_account.php
@@ -9,6 +9,17 @@
       $user_email=$row_fetch['user_email'];
       $user_address=$row_fetch['user_address'];
       $user_mobile=$row_fetch['user_mobile'];
+
+      if(isset($_POST['user_update'])){
+         $update_id=$user_id;
+         $username=$_POST['username'];
+         $user_email=$_POST['user_email'];
+         $user_address=$_POST['user_address'];
+         $user_mobile=$_POST['user_mobile'];
+         $user_image=$_FILES['user_image']['name'];
+         $user_image_tmp=$_FILES['user_image']['tmp'];
+         move_uploaded_file($user_image_tmp,"./user_images/$user_image");
+      }
    }
 ?>
 
@@ -23,20 +34,20 @@
     <h3 class="text-center text-success mb-4">Edit Account</h3>
     <form action="" method="post" enctype="multipart/form-data" class="text-center">
         <div class="form-outline mb-4">
-            <input type="text" class="form-control w-50 m-auto" name="user_username">
+            <input type="text" class="form-control w-50 m-auto" value="<?php echo $username; ?>" name="user_username">
         </div>
         <div class="form-outline mb-4">
-            <input type="email" class="form-control w-50 m-auto" name="user_email">
+            <input type="email" class="form-control w-50 m-auto" value="<?php echo $user_email; ?>" name="user_email">
         </div>
         <div class="form-outline mb-4 d-flex  w-50 m-auto">
             <input type="file" class="form-control" name="user_image">
             <img src="./user_images/<?php echo $user_image ?>" alt="" class="edit_image">
         </div>
         <div class="form-outline mb-4">
-            <input type="text" class="form-control w-50 m-auto" name="user_address">
+            <input type="text" class="form-control w-50 m-auto" name="user_address" value="<?php echo $user_address; ?>">
         </div>
         <div class="form-outline mb-4">
-            <input type="text" class="form-control w-50 m-auto" name="user_mobile">
+            <input type="text" class="form-control w-50 m-auto" name="user_mobile" value="<?php echo $user_mobile; ?>">
         </div>
         <input type="submit" value="Update" class="bg-info py-2 px-3 border-0" name="user_update">
     </form>
