c757ea7b4777169db03d2ecbc0e944742c3507cf
Suniksha <sunikshabenpatel@gmail.com>
2024-12-29 00:04:10 +0530

    Day-63 Building user payment page

--- Project-12 E-Commerce Website using PHP and MySQL/users_area/confirm_payment.php
+++ Project-12 E-Commerce Website using PHP and MySQL/users_area/confirm_payment.php
@@ -4,6 +4,60 @@
     @session_start();
     if(isset($_GET['order_id'])){
         $order_id = $_GET['order_id'];
-        echo $order_id;
+        //echo $order_id;
+        $select_data="SELECT * FROM `user_orders` WHERE order_id=$order_id";
+        $result = mysqli_query($con,$select_data);
+        $row_fetch=mysqli_fetch_assoc($result);
+        $invoice_number=$row_fetch['invoice_number'];
+        $amount_due=$row_fetch['amount_due'];
     }
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap css Link -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <!--FonT awesome link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/brands.min.css" integrity="sha512-7rXIvd/xj1FnI2zKQjjXzDFxC4twqBByp8yxOgEQJs4C/aNzNyoQsOO7VxH0RgYNhbAYLJLwzqslaP50KTTHIQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!--Css link -->
+    <link rel="stylesheet" href="../Project-12 E-Commerce Website using PHP and MySQL/css/style.css" class="css">
+    <title>Payment page</title>
+</head>
+
+<body class="bg-secondary">
+    <div class="container my-5">
+      <h1 class="text-center text-light">Confirm Payment</h1>
+        <form action="" method="post">
+            <div class="form-outline my-4 text-center w-50 m-auto">
+                <input type="text" class="form-control w-50 m-auto" name="invoice_number" value="<?php echo $invoice_number; ?>">
+            </div>
+            <div class="form-outline my-4 text-center w-50 m-auto">
+                <label for="" class="text-light">Amount</label>
+                <input type="text" class="form-control w-50 m-auto" name="amount" value="<?php echo $amount_due; ?>">
+            </div>
+            <div class="form-outline my-4 text-center w-50 m-auto">
+                <select name="payment_mode" class="form-select w-50 m-auto">
+                    <option>Select Payment Mode</option>
+                    <option>UPI</option>
+                    <option>Netbanking</option>
+                    <option>Paytm</option>
+                    <option>Paypal</option>
+                    <option>Cash On Delivery</option>
+                    <option>Payoffline</option>
+                </select>
+            </div>
+            <div class="form-outline my-4 text-center w-50 m-auto">
+                <input type="submit" class="bg-info py-2 px-3 border-0" value="Confirm" name="confirm_payment">
+            </div>
+        </form>
+    </div>
+    <!--script.js-->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+</body>
+
+</html>
