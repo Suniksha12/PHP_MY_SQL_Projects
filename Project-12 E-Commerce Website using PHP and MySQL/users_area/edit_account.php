7ffcf89c5f9b7caaf2748f091c1e4065fbf9665a
Suniksha <sunikshabenpatel@gmail.com>
2024-12-28 12:09:02 +0530

    Day-63 adding php code for edit account

--- Project-12 E-Commerce Website using PHP and MySQL/users_area/edit_account.php
+++ Project-12 E-Commerce Website using PHP and MySQL/users_area/edit_account.php
@@ -1,3 +1,17 @@
+<?php
+   if(isset($_GET['edit_account'])){
+      $user_session_name = $_SESSION['username'];
+      $select_query="SELECT * FROM `user_table` WHERE username='$user_session_name'";
+      $result_query=mysqli_query($con,$select_query);
+      $row_fetch=mysqli_fetch_assoc($result_query);
+      $user_id=$row_fetch['user_id'];
+      $username=$row_fetch['username'];
+      $user_email=$row_fetch['user_email'];
+      $user_address=$row_fetch['user_address'];
+      $user_mobile=$row_fetch['user_mobile'];
+   }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -14,8 +28,9 @@
         <div class="form-outline mb-4">
             <input type="email" class="form-control w-50 m-auto" name="user_email">
         </div>
-        <div class="form-outline mb-4">
-            <input type="file" class="form-control w-50 m-auto" name="user_image">
+        <div class="form-outline mb-4 d-flex  w-50 m-auto">
+            <input type="file" class="form-control" name="user_image">
+            <img src="./user_images/<?php echo $user_image ?>" alt="" class="edit_image">
         </div>
         <div class="form-outline mb-4">
             <input type="text" class="form-control w-50 m-auto" name="user_address">
