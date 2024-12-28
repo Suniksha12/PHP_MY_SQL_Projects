eec23e5bc6e48edeb3f7c7f114b2c2cc36d60155
Suniksha <sunikshabenpatel@gmail.com>
2024-12-28 15:39:31 +0530

    Day-63 buidling user order.php table

---
+++ Project-12 E-Commerce Website using PHP and MySQL/users_area/user_orders.php
@@ -0,0 +1,36 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h3 class="text-success">All My Orders</h3>
+    <table class="table table-bordered mt-5">
+        <thead class="bg-info">
+            <tr>
+                <th>Sl No</th>
+                <th>Amount Due</th>
+                <th>Total Products</th>
+                <th>Invoice Number</th>
+                <th>Date</th>
+                <th>Complete/Incomplete</th>
+                <th>Status</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>1</td>
+                <td>100</td>
+                <td>3</td>
+                <td>121132</td>
+                <td>21212</td>
+                <td>Complete</td>
+                <td>Confirm</td>
+            </tr>
+        </tbody>
+    </table>
+</body>
+</html>
