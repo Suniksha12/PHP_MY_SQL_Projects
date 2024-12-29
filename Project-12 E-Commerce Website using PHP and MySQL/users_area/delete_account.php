21070785c27e9c2249bf6bbc790412b584fc9064
Suniksha <sunikshabenpatel@gmail.com>
2024-12-29 19:31:38 +0530

    Day-64 user account deletion code

--- Project-12 E-Commerce Website using PHP and MySQL/users_area/delete_account.php
+++ Project-12 E-Commerce Website using PHP and MySQL/users_area/delete_account.php
@@ -22,6 +22,15 @@
    $username_session = $_SESSION['username'];
    if(isset($_POST['delete'])){
       $delete_query="DELETE FROM `user_table` WHERE username=='$username_session'";
-      
+      $result=mysqli_query($con,$delete_query);
+      if($result){
+          session_destroy();
+          echo "<script>alert('Account Deleted Successfully')</script>";
+          echo "<script>window.open('../index.php','_self')</script>";
+      }
+   }
+
+   if(isset($_POST['dont_delete'])){
+    echo "<script>window.open('profile.php','_self')</script>";
    }
 ?>
