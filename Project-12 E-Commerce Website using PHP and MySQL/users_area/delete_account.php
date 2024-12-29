6eaafc3f2f4d5dd030734ce166dcc28e2b7deb2f
Suniksha <sunikshabenpatel@gmail.com>
2024-12-29 19:19:52 +0530

    Day-64 writing the php code

--- Project-12 E-Commerce Website using PHP and MySQL/users_area/delete_account.php
+++ Project-12 E-Commerce Website using PHP and MySQL/users_area/delete_account.php
@@ -7,5 +7,21 @@
 </head>
 <body>
     <h3 class="text-danger mb-4">Delete Account</h3>
+    <form action="" method="post" class="mt-5">
+        <div class="form-outline mb-4">
+            <input type="submit" class="form-control w-50 m-auto" name="delete" value="Delete Account">
+        </div>
+        <div class="form-outline mb-4">
+            <input type="submit" class="form-control w-50 m-auto" name="dont_delete" value="Don't Delete Account">
+        </div>
+    </form>
 </body>
 </html>
+
+<?php 
+   $username_session = $_SESSION['username'];
+   if(isset($_POST['delete'])){
+      $delete_query="DELETE FROM `user_table` WHERE username=='$username_session'";
+      
+   }
+?>
