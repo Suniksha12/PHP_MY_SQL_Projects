0b069df53292e284c68c67064a79090be2535e3b
Suniksha <sunikshabenpatel@gmail.com>
2024-12-30 16:38:27 +0530

    Day-65 buiding edit forn

--- Project-12 E-Commerce Website using PHP and MySQL/admin_area/edit_products.php
+++ Project-12 E-Commerce Website using PHP and MySQL/admin_area/edit_products.php
@@ -14,6 +14,7 @@
             <input type="text" id="product_keywords" name="product_keywords" class="form-control" required>
         </div>
         <div class="form-outline w-50 m-auto mb-4">
+            <label for="product_category" class="form-label">Product Categories</label>
             <select name="product_category" id="" class="form-select">
                 <option value="">1</option>
                 <option value="">1</option>
@@ -23,6 +24,7 @@
             </select>
         </div>
         <div class="form-outline w-50 m-auto mb-4">
+            <label for="product_brands" class="form-label">Product Brands</label>
             <select name="product_brands" id="" class="form-select">
                 <option value="">1</option>
                 <option value="">1</option>
@@ -30,6 +32,34 @@
                 <option value="">1</option>
                 <option value="">1</option>
             </select>
+        </div>
+        <div class="form-outline w-50 m-auto mb-4">
+            <label for="product_image1" class="form-label">Product Image1</label>
+            <div class="d-flex">
+                <input type="file" id="product_image1" name="product_image1" class="form-control w-90 m-auto" required>
+                <img src="../images/apple.png" alt="" class="product_img">
+            </div>
+        </div>
+        <div class="form-outline w-50 m-auto mb-4">
+            <label for="product_image2" class="form-label">Product Image2</label>
+            <div class="d-flex">
+                <input type="file" id="product_image2" name="product_image2" class="form-control w-90 m-auto" required>
+                <img src="../images/mango.jpg" alt="" class="product_img">
+            </div>
+        </div>
+        <div class="form-outline w-50 m-auto mb-4">
+            <label for="product_image3" class="form-label">Product Image3</label>
+            <div class="d-flex">
+                <input type="file" id="product_image3" name="product_image3" class="form-control w-90 m-auto" required>
+                <img src="../images/capsi1.webp" alt="" class="product_img">
+            </div>
+        </div>
+        <div class="form-outline w-50 m-auto mb-4">
+            <label for="product_price" class="form-label">Product Price</label>
+            <input type="text" id="product_price" name="product_price" class="form-control" required>
+        </div>
+        <div class="text-center">
+            <input type="submit" name="edit_product" name="edit_product" value="Update product" class="btn btn-info px-3 mb-3">
         </div>
     </form>
 </div>
