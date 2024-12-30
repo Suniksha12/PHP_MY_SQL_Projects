31b676a2281ab9b341900fb5f0c9093b3945c4b2
Suniksha <sunikshabenpatel@gmail.com>
2024-12-30 20:41:46 +0530

    Day-65 admin upadting products data

--- Project-12 E-Commerce Website using PHP and MySQL/admin_area/edit_products.php
+++ Project-12 E-Commerce Website using PHP and MySQL/admin_area/edit_products.php
@@ -21,7 +21,7 @@
       $result_category=mysqli_query($con,$select_category);
       $row_category=mysqli_fetch_assoc($result_category);
       $category_title=$row_category['category_title'];
-    //   echo $category_title;
+      //echo $category_title;
 
       //fetching brand name
       $select_brand="SELECT * FROM `brands` WHERE brand_id=$brand_id";
@@ -116,7 +116,7 @@
 <?php 
     if(isset($_POST['edit_product'])){
         $product_title = $_POST['product_title'];
-        $product_description = $_POST['product_description'];
+        $product_description = $row['product_description'];
         $product_keywords = $_POST['product_keywords'];
         $product_category = $_POST['product_category'];
         $product_brands = $_POST['product_brands'];
@@ -134,7 +134,7 @@
         $temp_image3 = $_FILES['product_image3']['tmp_name'];
 
         //checking for fields empty or not
-        if($product_title=='' or $product_desc=='' or $product_keywords=='' or 
+        if($product_title=='' or $product_description=='' or $product_keywords=='' or 
            $product_category=='' or $product_brands=='' or $product_image1=='' or 
            $product_image2=='' or $product_image3=='' or $product_price==''){     
             echo "<script>alert('Please fill all the fields and continue the process')</script>";
