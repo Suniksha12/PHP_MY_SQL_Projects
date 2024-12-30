700b0736578f26670d3b28399b6653ee4cfbce12
Suniksha <sunikshabenpatel@gmail.com>
2024-12-30 11:33:46 +0530

    Day-65 building edit products.php

---
+++ Project-12 E-Commerce Website using PHP and MySQL/admin_area/edit_products.php
@@ -0,0 +1,8 @@
+<div class="container mt-5">
+    <h1 class="text-center">Edit Product</h1>
+    <form action="" method="post" enctype="multipart/form-data">
+        <div class="form-outline">
+            <label for="product_title" class="form-label">Product Title</label>
+        </div>
+    </form>
+</div>
