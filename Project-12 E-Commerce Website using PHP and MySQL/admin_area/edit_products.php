a39490d3cec5389c51a35a871d1fcc5042b0880f
Suniksha <sunikshabenpatel@gmail.com>
2024-12-30 12:00:26 +0530

    Day-65 making form edit products

--- Project-12 E-Commerce Website using PHP and MySQL/admin_area/edit_products.php
+++ Project-12 E-Commerce Website using PHP and MySQL/admin_area/edit_products.php
@@ -1,8 +1,35 @@
 <div class="container mt-5">
     <h1 class="text-center">Edit Product</h1>
     <form action="" method="post" enctype="multipart/form-data">
-        <div class="form-outline">
+        <div class="form-outline w-50 m-auto mb-4">
             <label for="product_title" class="form-label">Product Title</label>
+            <input type="text" id="product_title" name="product_title" class="form-control" required>
+        </div>
+        <div class="form-outline w-50 m-auto mb-4">
+            <label for="product_desc" class="form-label">Product Description</label>
+            <input type="text" id="product_desc" name="product_desc" class="form-control" required>
+        </div>
+        <div class="form-outline w-50 m-auto mb-4">
+            <label for="product_keywords" class="form-label">Product Keywords</label>
+            <input type="text" id="product_keywords" name="product_keywords" class="form-control" required>
+        </div>
+        <div class="form-outline w-50 m-auto mb-4">
+            <select name="product_category" id="" class="form-select">
+                <option value="">1</option>
+                <option value="">1</option>
+                <option value="">1</option>
+                <option value="">1</option>
+                <option value="">1</option>
+            </select>
+        </div>
+        <div class="form-outline w-50 m-auto mb-4">
+            <select name="product_brands" id="" class="form-select">
+                <option value="">1</option>
+                <option value="">1</option>
+                <option value="">1</option>
+                <option value="">1</option>
+                <option value="">1</option>
+            </select>
         </div>
     </form>
 </div>
